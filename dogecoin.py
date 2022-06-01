2f92bc7040cfd9c841a286b6a3a17c7cd7a69a2e
Mathilde Haglund <mathilde.haglund@outlook.no>
2022-06-01 20:25:18 +0200

    third commit

--- dogecoin.py
+++ dogecoin.py
@@ -1,10 +1,29 @@
-import streamlit as sl
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from pycoingecko import CoinGeckoAPI
+from datetime import datetime, timedelta
 
-sl.write("""
-# Heii Jørgen
-Under her er noen punkter
- - punkt
- - punkt
- - punkt""")
+cg = CoinGeckoAPI()
+
+st.write("# FOMO kalkulator")
+
+price_now = cg.get_price(ids="dogecoin", vs_currencies="usd")["dogecoin"]["usd"]
+
+st.write("Velg Dato, og Beløp")
+
+todays_date = datetime.utcnow().date()
+hist_date = st.date_input("Date:")
+initial_amount = st.number_input("Beløp")
+HIST_DATE_REFORMAT = hist_date.strftime("%d-%m-%Y")
+
+doge_historic = cg.get_coin_history_by_id(id="dogecoin", vs_currencies="usd", date=HIST_DATE_REFORMAT)['market_data']['current_price']['usd']
+
+st.write("Historical FOMO Analysis")
+total = initial_amount/doge_historic
+value_now = total * price_now
+
+st.write("Your dogecoin are worth:", value_now)
+
+change_percent = ((value_now - initial_amount)/(initial_amount)) * 100
+st.write("The change in percent is:", change_percent)
